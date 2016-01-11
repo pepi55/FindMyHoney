17508bb21923872b4d46a91d8d350602a8ece5b6
Petar Dimitrov <pepi55@live.com>
2016-01-11 21:16:05 +0100

    Commented code.

--- FindMyHoney/GameObject.h
+++ FindMyHoney/GameObject.h
@@ -2,11 +2,15 @@
 
 #include "stdafx.h"
 
+// Base class all objects in the game will be derived from.
+// The gameobject class has virtual functions load and draw
+// each of which are used to load an image from a file and
+// draw the image on screen respectively.
 class GameObject
 {
 	public:
-		GameObject();
-		virtual ~GameObject();
+		GameObject(void);
+		virtual ~GameObject(void);
 
 		virtual void load(std::string name);
 		virtual void draw(sf::RenderWindow &window);
