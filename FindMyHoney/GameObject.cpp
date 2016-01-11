4d5b8969bb928f4b3b9bccbc5a76cae124b7829c
Petar Dimitrov <pepi55@live.com>
2016-01-11 21:16:19 +0100

    Implemented GameObject class.

---
+++ FindMyHoney/GameObject.cpp
@@ -0,0 +1,42 @@
+#include "GameObject.h"
+#include "stdafx.h"
+
+GameObject::GameObject(void)
+	: isLoaded(false)
+{
+}
+
+GameObject::~GameObject(void)
+{
+}
+
+void GameObject::load(std::string name)
+{
+	if (texture.loadFromFile(name) == false)
+	{
+		filename = "";
+		isLoaded = false;
+	}
+	else
+	{
+		filename = name;
+		sprite.setTexture(texture);
+		isLoaded = true;
+	}
+}
+
+void GameObject::draw(sf::RenderWindow &window)
+{
+	if (isLoaded)
+	{
+		window.draw(sprite);
+	}
+}
+
+void GameObject::setPosition(float x, float y)
+{
+	if (isLoaded)
+	{
+		sprite.setPosition(x, y);
+	}
+}
